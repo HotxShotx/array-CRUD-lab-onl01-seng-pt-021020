d640c4dd22fb0eabf27ef8f777cf6d8416253216
Omar B <omar.benjamin377@gmail.com>
2020-02-11 02:56:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array = ["re", "fire", "hot", "arrays!"]
+  create_an_array = ["wow", "fire", "hot", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
